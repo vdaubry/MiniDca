03dea54893e23483e6a387a15864f66d9d2ae2ce
Vincent Daubry <vdaubry@gmail.com>
2022-11-26 21:55:21 +0100

    get contract adress

--- frontend/components/Funding.js
+++ frontend/components/Funding.js
@@ -1,14 +1,30 @@
 import { useWeb3Contract } from "react-moralis";
+import { abi, contractAddresses } from "../constants";
+import { useMoralis } from "react-moralis";
 
 export default function Funding() {
-  const { data, error, runContractFunction, isFetching, isLoading } =
-    useWeb3Contract({
-      abi: usdcEthPoolAbi,
-      contractAddress: usdcEthPoolAddress,
-      functionName: "fund",
-      params: {},
-      msgValue: 0.1,
-    });
+  const { chainId: chainIdHex } = useMoralis();
+  if (chainIdHex != null) {
+    const chainId = parseInt(chainIdHex);
+    const dcaAddress = contractAddresses[chainId]["dca"];
 
-  return <div>Foobar</div>;
+    // const {
+    //   data,
+    //   error,
+    //   runContractFunction: fundContract,
+    //   isFetching,
+    //   isLoading,
+    // } = useWeb3Contract({
+    //   contractAddress: dcaAddress,
+    //   functionName: "fund",
+    //   params: {},
+    //   msgValue: 0.1,
+    // });
+  }
+
+  return (
+    <div>
+      <button>Fund contract</button>
+    </div>
+  );
 }
