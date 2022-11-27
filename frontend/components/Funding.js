d042a92bc9470d52687f805f53394e386fbb4b3a
Vincent Daubry <vdaubry@gmail.com>
2022-11-27 15:39:32 +0100

    add spinner when tx is pending

--- frontend/components/Funding.js
+++ frontend/components/Funding.js
@@ -22,8 +22,6 @@ export default function Funding() {
    **************************************/
 
   const {
-    data,
-    error,
     runContractFunction: fundContract,
     isFetching,
     isLoading,
@@ -97,6 +95,7 @@ export default function Funding() {
           <div>
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              disabled={isFetching || isLoading}
               onClick={async () => {
                 await fundContract({
                   // onComplete:
@@ -107,7 +106,11 @@ export default function Funding() {
                 console.log("Funding contract");
               }}
             >
-              Fund contract
+              {isLoading || isFetching ? (
+                <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
+              ) : (
+                <div>Fund contract</div>
+              )}
             </button>
           </div>
         </div>
