20f1373406b201234a892071b661b50f19d65e74
Vincent Daubry <vdaubry@gmail.com>
2022-11-28 08:24:44 +0100

    add gas-report

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -3,11 +3,13 @@ require("hardhat-deploy");
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require("solidity-coverage");
+require("solidity-coverage");
 require("dotenv").config();
 
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -29,6 +31,13 @@ module.exports = {
     apiKey: {
       goerli: ETHERSCAN_API_KEY,
     },
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   namedAccounts: {
     deployer: {
