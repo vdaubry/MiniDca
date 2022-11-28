3df9359bee93b95f809381476aff1ad657382fd8
Vincent Daubry <vdaubry@gmail.com>
2022-11-28 17:49:08 +0100

    refactor smart contract to receive USDC instead of ETH

--- smart_contracts/contracts/Dca.sol
+++ smart_contracts/contracts/Dca.sol
@@ -2,32 +2,39 @@
 pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 error Dca__WithdrawError();
 
 contract Dca {
-    mapping(address => uint256) s_addressToAmountInvested;
+    mapping(address => uint256) s_addressToAmountDeposited;
+    IERC20 private s_usdc;
 
-    constructor() {}
+    constructor(address usdcAddress) {
+        s_usdc = IERC20(usdcAddress);
+    }
 
-    function fund() public payable {
-        s_addressToAmountInvested[msg.sender] += msg.value;
+    function deposit(uint256 depositAmount) public {
+        require(depositAmount > 0);
+
+        uint256 formatedDepositAmount = depositAmount * 10 ** 6; //USDC has 6 decimals
+        s_usdc.transferFrom(msg.sender, address(this), formatedDepositAmount);
+        s_addressToAmountDeposited[msg.sender] += formatedDepositAmount;
     }
 
     function withdraw() public {
-        uint256 amountToWithdraw = s_addressToAmountInvested[msg.sender];
+        uint256 amountToWithdraw = s_addressToAmountDeposited[msg.sender];
         if (amountToWithdraw <= 0) {
             return;
         }
 
-        s_addressToAmountInvested[msg.sender] = 0;
-        (bool success, ) = msg.sender.call{value: amountToWithdraw}("");
-        if (!success) revert Dca__WithdrawError();
+        s_addressToAmountDeposited[msg.sender] = 0;
+        s_usdc.transfer(msg.sender, amountToWithdraw);
     }
 
     function getAmountInvestedForAddress(
         address investor
     ) public view returns (uint256) {
-        return s_addressToAmountInvested[investor];
+        return s_addressToAmountDeposited[investor];
     }
 }
