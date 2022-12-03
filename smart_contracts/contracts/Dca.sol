59b306e170a53770939c3dc3827452afe06d5769
Vincent Daubry <vdaubry@gmail.com>
2022-12-03 21:33:00 +0100

    Add more smart contract checks

--- smart_contracts/contracts/Dca.sol
+++ smart_contracts/contracts/Dca.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+error Dca__DepositError();
 error Dca__WithdrawError();
 
 contract Dca {
@@ -15,18 +16,29 @@ contract Dca {
     }
 
     function deposit(uint256 depositAmount) public {
-        require(depositAmount > 0);
+        if (depositAmount <= 0) revert Dca__DepositError();
 
         uint256 formatedDepositAmount = depositAmount * 10 ** 6; //USDC has 6 decimals
-        s_usdc.transferFrom(msg.sender, address(this), formatedDepositAmount);
+
+        // Check if the caller has sufficient funds to deposit
+        require(
+            s_usdc.balanceOf(msg.sender) >= formatedDepositAmount,
+            "Insufficient funds"
+        );
+
+        bool callSuccess = s_usdc.transferFrom(
+            msg.sender,
+            address(this),
+            formatedDepositAmount
+        );
+        require(callSuccess, "Transfer failed");
+
         s_addressToAmountDeposited[msg.sender] += formatedDepositAmount;
     }
 
     function withdraw() public {
         uint256 amountToWithdraw = s_addressToAmountDeposited[msg.sender];
-        if (amountToWithdraw <= 0) {
-            return;
-        }
+        if (amountToWithdraw <= 0) revert Dca__WithdrawError();
 
         s_addressToAmountDeposited[msg.sender] = 0;
         s_usdc.transfer(msg.sender, amountToWithdraw);
