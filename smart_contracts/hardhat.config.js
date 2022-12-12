d5650fe15c850ae4d6ffae04a9c03f6bd4a50f48
Vincent Daubry <vdaubry@gmail.com>
2022-12-12 17:16:06 +0100

    add swap contract

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -20,9 +20,9 @@ module.exports = {
       {
         version: "0.8.17",
       },
-      // {
-      //   version: "0.7.6",
-      // },
+      {
+        version: "0.8.0",
+      },
     ],
   },
   defaultNetwork: "hardhat",
