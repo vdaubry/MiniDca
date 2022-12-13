0944b898415f164d75d75e5fafdceeb0836db9aa
Vincent Daubry <vdaubry@gmail.com>
2022-12-13 07:25:35 +0100

    fork mainnet to get contract balance

--- smart_contracts/helper-hardhat-config.js
+++ smart_contracts/helper-hardhat-config.js
@@ -5,6 +5,7 @@ const networkConfig = {
   31337: {
     name: "localhost",
     wethToken: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+    usdcToken: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
   },
   5: {
     name: "goerli",
