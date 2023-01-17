05ba9a5d212cb9c4b5e26fb82530860f2c5766b3
Vincent Daubry <vdaubry@gmail.com>
2023-01-17 06:15:58 +0100

    rename approve function

--- frontend/components/ApproveToken.js
+++ frontend/components/ApproveToken.js
@@ -20,7 +20,12 @@ export default function ApproveToken({ dcaAddress, usdcAddress }) {
     functionName: "approve",
     args: [dcaAddress, ethers.constants.MaxInt256],
   });
-  const { tx, isLoading, isSuccess, write } = useContractWrite({
+  const {
+    tx,
+    isLoading,
+    isSuccess,
+    write: approveUsdc,
+  } = useContractWrite({
     ...config,
     onError(error) {
       console.log(error);
@@ -97,8 +102,8 @@ export default function ApproveToken({ dcaAddress, usdcAddress }) {
         <div>
           <button
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            disabled={!write}
-            onClick={() => write?.()}
+            disabled={!approveUsdc}
+            onClick={() => approveUsdc?.()}
           >
             {isLoading ? (
               <div className="animate-spin spinner-border h-8 w-8 border-b-2 rounded-full"></div>
