76e72581f12492ebf491a6f8013600449ed660ea
Vincent Daubry <vdaubry@gmail.com>
2023-01-30 14:41:11 +0100

    deploy on goerli

--- frontend/utils/wagmi.js
+++ frontend/utils/wagmi.js
@@ -1,8 +1,8 @@
-import { mainnet, polygon, arbitrum, hardhat } from "wagmi/chains";
+import { mainnet, polygon, arbitrum, hardhat, goerli } from "wagmi/chains";
 import { getDefaultClient } from "connectkit";
 import { createClient } from "wagmi";
 
-const chains = [mainnet, polygon, arbitrum, hardhat];
+const chains = [mainnet, polygon, arbitrum, goerli, hardhat];
 
 export const client = createClient(
   getDefaultClient({
